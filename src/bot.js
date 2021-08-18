9d4cb6437c2a168df2164cb16f7c5cbd84c99f8e
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2021-08-18 08:06:09 +0530

    bot made online

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-console.log(process.env.DISCORD_BOT_TOKEN);
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
